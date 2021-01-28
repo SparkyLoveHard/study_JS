c64dca63f5284f517962b88663bfa212505201a1
Your Name <sparky.1995@mail.ru>
2021-01-28 21:08:07 +0500

    add first lesson

---
+++ lesson01/script.js
@@ -0,0 +1,10 @@
+let money; 
+let income;
+let addExpenses;
+let deposit;
+let mission; 
+let period;
+
+alert(money, income, addExpenses, deposit, mission, period);
+
+console.log(money, income, addExpenses, deposit, mission, period);
