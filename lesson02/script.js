f1422fbb787c42069574e2a00d79f2f5e72a5f24
Your Name <sparky.1995@mail.ru>
2021-01-29 14:34:45 +0500

    add second lesson

---
+++ lesson02/script.js
@@ -0,0 +1,15 @@
+let money = 30000;
+let income = 'фриланс';
+let addExpenses = 'интернет, такси, коммуналка, еда';
+let despoit = true;
+let mission = 10000;
+let period = 4;
+let budgetDay = money / 30;
+
+console.log(typeof(money), typeof(income), typeof(despoit));
+console.log(addExpenses.length);
+console.log('Период равен' + ' ' + period + ' ' + 'месяца');
+console.log('Цель заработать' + ' ' + mission + ' ' + 'рублей');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+console.log(budgetDay);
