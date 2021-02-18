cbec1a5e6bb003dae65e20bf0e4d5e6ad1bb4e9c
Your Name <sparky.1995@mail.ru>
2021-02-18 15:41:16 +0500

    fix lesson 12

--- lesson12/script.js
+++ lesson12/script.js
@@ -4,16 +4,19 @@ const headerInput = document.querySelector('.header-input');
 const todoList = document.querySelector('.todo-list');
 const todoCompleted = document.querySelector('.todo-completed');
 
-const todoData = [
+let todoData = [
     // {
     //     value: 'Помыть посуду',
     //     completed: true,
     // }
 ];
 
+
 function render() {
     todoList.textContent = '';
     todoCompleted.textContent = '';
+
+    todoData = JSON.parse(localStorage.getItem('todoList')) || [];
 
     todoData.forEach(function(item) {
         const li = document.createElement('li');
@@ -24,18 +27,22 @@ function render() {
             <button class="todo-remove"></button>
             <button class="todo-complete"></button>
         </div>`;
-
+        
         if(item.completed) {
             todoCompleted.append(li);
         } else {
             todoList.append(li);
         }
 
-        const btnTodoCompleted = li.querySelector('.todo-complete');
+        
 
+        const btnTodoCompleted = li.querySelector('.todo-complete');
+        
         btnTodoCompleted.addEventListener('click', function() {
             // если был true станет false (!item.completed)
             item.completed = !item.completed;
+
+            localStorage.setItem('todoList', JSON.stringify(todoData));
             render();
         });
 
@@ -44,7 +51,9 @@ function render() {
         btnTodoRemove.addEventListener('click', function() {
             //todoData = (todoData.indexOf(item));
             todoData.splice(todoData.indexOf(item), 1);
-            localStorage.setItem('plans', JSON.stringify(todoData));
+
+            localStorage.setItem('todoList', JSON.stringify(todoData));
+
             render();
         });
     });
@@ -61,21 +70,22 @@ todoControl.addEventListener('submit', function(event) {
     // Пустые дела добавляться не должны
     if(headerInput.value !== '') {
         todoData.push(newTodo);
-        // headerInput.value
-       
-        localStorage.setItem('plans', JSON.stringify(todoData));
-        
-
+        localStorage.setItem('todoList', JSON.stringify(todoData));
         render();
         // Поле ввода после добавления дела должно очищаться
         headerInput.value = '';
     } else {
         console.log('empty str');
     }
-
-
-    
 });
+
+
+// function showLi() {
+//     todoList.innerHTML = localStorage.getItem('todoList');
+//     console.log(todoList.innerHTML);
+// }
+// showLi()
+
 
 render();   
 
